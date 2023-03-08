f54d35fa3b8469c5617325c5a8c5ba80368a404a
Ahmedkhaledhassan <117466876+Ahmedkhaledhassan@users.noreply.github.com>
2023-03-08 10:56:04 -0800

    milk sales code

--- Diary_Farm_MS/Cows.cs
+++ Diary_Farm_MS/Cows.cs
@@ -47,5 +47,19 @@ namespace Diary_Farm_MS
             Ob.Show();
             this.Hide();
         }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            Breadings Ob = new Breadings();
+            Ob.Show();
+            this.Hide();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            MilkSales Ob = new MilkSales();
+            Ob.Show();
+            this.Hide();
+        }
     }
 }
