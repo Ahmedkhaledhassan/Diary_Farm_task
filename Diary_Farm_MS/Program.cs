8514376a779b19de563691145517b2e2ec1dcbfe
Ahmedkhaledhassan <117466876+Ahmedkhaledhassan@users.noreply.github.com>
2023-03-08 07:29:12 -0800

    Edited errors in programms page

--- Diary_Farm_MS/Program.cs
+++ Diary_Farm_MS/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Diary_Farm_MS
 {
@@ -12,14 +13,13 @@ namespace Diary_Farm_MS
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+        [STAThread]
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
-            {
-                new Service1()
-            };
-            ServiceBase.Run(ServicesToRun);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login_page());
         }
     }
 }
