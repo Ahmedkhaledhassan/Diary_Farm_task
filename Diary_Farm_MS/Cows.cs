b69ddbb400d630a2ed3a4e663c971ea6d5a558c5
Ahmedkhaledhassan <117466876+Ahmedkhaledhassan@users.noreply.github.com>
2023-03-08 10:56:49 -0800

    finance

--- Diary_Farm_MS/Cows.cs
+++ Diary_Farm_MS/Cows.cs
@@ -61,5 +61,12 @@ namespace Diary_Farm_MS
             Ob.Show();
             this.Hide();
         }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            Finances Ob = new Finances();
+            Ob.Show();
+            this.Hide();
+        }
     }
 }
